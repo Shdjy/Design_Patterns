54dd19a66bf8ee50056871320db84540c5ae6497
Shdjy <sunhaodong710@gmail.com>
2024-01-20 11:37:43 +0800

    单例模式，c++11

---
+++ Singleton3/main.cpp
@@ -0,0 +1,19 @@
+﻿#include "Singleton.h"
+#include <thread>
+
+int main()
+{
+	cout << "主函数开始" << endl;
+	thread t1([]() {
+		Singleton& s1 = Singleton::getInstance();
+		});
+
+	thread t2([]() {
+		Singleton& s2 = Singleton::getInstance();
+		});
+	t1.join();
+	t2.join();
+
+	cout << "主函数结束" << endl;
+	return 0;
+}
